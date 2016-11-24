a22695543a576b2ffe2c82771a47648836a9be95
shark5 <panj0525@sina.com>
2016-11-24 17:38:40 +0800

    screenshot

--- app/src/main/java/com/cjp/crosswalk/MainActivity.java
+++ app/src/main/java/com/cjp/crosswalk/MainActivity.java
@@ -1,27 +1,81 @@
 package com.cjp.crosswalk;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static String TAG = "MainActivity";
+    private final int REQUEST_CODE_SAVE_IMAGE_FILE = 110;
+
     private XWalkView xWalkWebView;
-    private ImageView mImage;
-    private Button screenShotBtn;
-    private Button recordBtn;
+    private ImageView mImageView;
+    private Button mScreenShotBtn;
+    private Button mRecordBtn;
+    private String mImageName;
+    private String mImagePath;
+    private int mScreenDensity;
+    private int mWindowWidth;
+    private int mWindowHeight;
+    private VirtualDisplay mVirtualDisplay;
+    private WindowManager mWindowManager;
+    private ImageReader mImageReader;
+
+    private MediaProjectionManager mMediaProjectionManager;
+    private MediaProjection mMediaProjection;
+
+    private static final String STATE_RESULT_CODE = "result_code";
+    private static final String STATE_RESULT_DATA = "result_data";
+
+    private static final int REQUEST_MEDIA_PROJECTION = 1;
+
+    private int mResultCode;
+    private Intent mResultData;
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mResultCode = savedInstanceState.getInt(STATE_RESULT_CODE);
+            mResultData = savedInstanceState.getParcelable(STATE_RESULT_DATA);
+        }
 
         xWalkWebView = (XWalkView) findViewById(R.id.xwalkWebView);
         xWalkWebView.load("https://crosswalk-project.org", null);
@@ -32,10 +86,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // load local html file
 //        xWalkWebView.load("http://www.w3.org/2010/05/video/mediaevents.html", null);
 
-        screenShotBtn = (Button) findViewById(R.id.btn_screenshot);
-        screenShotBtn.setOnClickListener(this);
-        mImage = (ImageView) findViewById(R.id.img);
+        mScreenShotBtn = (Button) findViewById(R.id.btn_screenshot);
+        mScreenShotBtn.setOnClickListener(this);
+        mRecordBtn = (Button) findViewById(R.id.btn_record);
+        mRecordBtn.setOnClickListener(this);
+        mImageView = (ImageView) findViewById(R.id.img);
 
+        createEnvironment();
     }
 
     @Override
@@ -62,10 +119,213 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (xWalkWebView != null) {
             xWalkWebView.onDestroy();
         }
+        if (mBitmap != null) {
+            mBitmap.recycle();
+            mBitmap = null;
+        }
     }
 
     @Override
     public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_screenshot) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "start startVirtual");
+                    startScreenCapture();
+                }
+            }, 500);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "start startCapture");
+                    startCapture();
+                }
+            }, 1000);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "start stopVirtual");
+                    stopScreenCapture();
+                }
+            }, 1500);
+        } else if (id == R.id.btn_record) {
+
+        }
     }
 
+    private void createEnvironment() {
+        mImagePath = Environment.getExternalStorageDirectory().getPath() + "/crosswalk/screenshot/";
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        mWindowWidth = mWindowManager.getDefaultDisplay().getWidth();
+        mWindowHeight = mWindowManager.getDefaultDisplay().getHeight();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        mWindowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenDensity = displayMetrics.densityDpi;
+        mImageReader = ImageReader.newInstance(mWindowWidth, mWindowHeight, 0x1, 2);
+
+        mMediaProjectionManager = (MediaProjectionManager)
+                getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+    }
+
+    private void startCapture() {
+        mImageName = System.currentTimeMillis() + ".png";
+        Log.i(TAG, "image name is : " + mImageName);
+        Image image = mImageReader.acquireLatestImage();
+        if (image == null) {
+            Log.e(TAG, "image is null.");
+            return;
+        }
+        int width = image.getWidth();
+        int height = image.getHeight();
+        final Image.Plane[] planes = image.getPlanes();
+        final ByteBuffer buffer = planes[0].getBuffer();
+        int pixelStride = planes[0].getPixelStride();
+        int rowStride = planes[0].getRowStride();
+        int rowPadding = rowStride - pixelStride * width;
+        mBitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+        mBitmap.copyPixelsFromBuffer(buffer);
+        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, width, height);
+        image.close();
+
+        if (mBitmap != null) {
+            Log.d(TAG, "bitmap create success ");
+            if (mImageView != null) {
+                mImageView.setImageBitmap(mBitmap);
+            }
+            checkPermission();
+        }
+    }
+
+    private void saveToFile() {
+        try {
+            File fileFolder = new File(mImagePath);
+            if (!fileFolder.exists())
+                fileFolder.mkdirs();
+            File file = new File(mImagePath, mImageName);
+            if (!file.exists()) {
+                Log.d(TAG, "file create success ");
+                file.createNewFile();
+            }
+            FileOutputStream out = new FileOutputStream(file);
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.flush();
+            out.close();
+            Log.d(TAG, "file save success ");
+            Toast.makeText(this.getApplicationContext(), "Screenshot is done.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (AndroidUtil.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_SAVE_IMAGE_FILE);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_SAVE_IMAGE_FILE);
+                }
+                return;
+            } else {
+                saveToFile();
+            }
+        } else {
+            saveToFile();
+        }
+    }
+
+    private void startScreenCapture() {
+        if (this == null) {
+            return;
+        }
+        if (mMediaProjection != null) {
+            setUpVirtualDisplay();
+        } else if (mResultCode != 0 && mResultData != null) {
+            setUpMediaProjection();
+            setUpVirtualDisplay();
+        } else {
+            Log.d(TAG, "Requesting confirmation");
+            // This initiates a prompt dialog for the user to confirm screen projection.
+            startActivityForResult(
+                    mMediaProjectionManager.createScreenCaptureIntent(),
+                    REQUEST_MEDIA_PROJECTION);
+        }
+    }
+
+    private void setUpMediaProjection() {
+        mMediaProjection = mMediaProjectionManager.getMediaProjection(mResultCode, mResultData);
+    }
+
+    private void setUpVirtualDisplay() {
+        mVirtualDisplay = mMediaProjection.createVirtualDisplay("ScreenCapture",
+                mWindowWidth, mWindowHeight, mScreenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mImageReader.getSurface(), null, null);
+    }
+
+    private void stopScreenCapture() {
+        if (mVirtualDisplay == null) {
+            return;
+        }
+        mVirtualDisplay.release();
+        mVirtualDisplay = null;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mResultData != null) {
+            outState.putInt(STATE_RESULT_CODE, mResultCode);
+            outState.putParcelable(STATE_RESULT_DATA, mResultData);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_MEDIA_PROJECTION) {
+            if (resultCode != Activity.RESULT_OK) {
+                Log.d(TAG, "User cancelled");
+                Toast.makeText(this, "User cancelled", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (this == null) {
+                return;
+            }
+            Log.d(TAG, "Starting screen capture");
+            mResultCode = resultCode;
+            mResultData = data;
+            setUpMediaProjection();
+            setUpVirtualDisplay();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e(TAG, "start startCapture");
+                    startCapture();
+                }
+            }, 1000);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_SAVE_IMAGE_FILE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    saveToFile();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+        }
+    }
 }
