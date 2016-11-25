fb788ccd08f0b36fbc0e9488cca622c719aece96
shark5 <panj0525@sina.com>
2016-11-25 10:55:48 +0800

    record

--- app/src/main/java/com/cjp/crosswalk/MainActivity.java
+++ app/src/main/java/com/cjp/crosswalk/MainActivity.java
@@ -10,18 +10,22 @@ import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.Image;
 import android.media.ImageReader;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -35,7 +39,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -66,6 +70,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int mResultCode;
     private Intent mResultData;
     private Bitmap mBitmap;
+
+    // record
+    private String mVideoPath;
+    private Surface mSurface;
+    private MediaCodec mMediaCodec;
+    private MediaMuxer mMuxer;
+    private boolean isRecordOn;
+
+    private AtomicBoolean mIsQuit = new AtomicBoolean(false);
+    private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
+    private boolean mMuxerStarted = false;
+    private int mVideoTrackIndex = -1;
+
+    private boolean isScreenshot = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,36 +141,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mBitmap.recycle();
             mBitmap = null;
         }
+        release();
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+        }
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.btn_screenshot) {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Log.e(TAG, "start startVirtual");
-                    startScreenCapture();
-                }
-            }, 500);
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Log.e(TAG, "start startCapture");
-                    startCapture();
-                }
-            }, 1000);
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Log.e(TAG, "start stopVirtual");
-                    stopScreenCapture();
-                }
-            }, 1500);
+            isScreenshot = true;
+            if (startScreenCapture()) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e(TAG, "start startCapture");
+                        startCapture();
+                    }
+                }, 200);
+            }
         } else if (id == R.id.btn_record) {
-
+            isScreenshot = false;
+            checkPermission();
         }
     }
 
@@ -168,6 +180,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mMediaProjectionManager = (MediaProjectionManager)
                 getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        mVideoPath = Environment.getExternalStorageDirectory().getPath() + "/crosswalk/record/";
     }
 
     private void startCapture() {
@@ -190,6 +204,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, width, height);
         image.close();
 
+        stopScreenCapture();
         if (mBitmap != null) {
             Log.d(TAG, "bitmap create success ");
             if (mImageView != null) {
@@ -231,28 +246,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 return;
             } else {
-                saveToFile();
+                if (isScreenshot) {
+                    saveToFile();
+                } else {
+                    recordClick();
+                }
             }
         } else {
-            saveToFile();
+            if (isScreenshot) {
+                saveToFile();
+            } else {
+                recordClick();
+            }
         }
     }
 
-    private void startScreenCapture() {
+    private boolean startScreenCapture() {
+        Log.e("0525", "startScreenCapture");
         if (this == null) {
-            return;
+            return false;
         }
         if (mMediaProjection != null) {
+            Log.e("0525", "startScreenCapture 1");
             setUpVirtualDisplay();
+            return true;
         } else if (mResultCode != 0 && mResultData != null) {
+            Log.e("0525", "startScreenCapture 2");
             setUpMediaProjection();
             setUpVirtualDisplay();
+            return true;
         } else {
+            Log.e("0525", "startScreenCapture 3");
             Log.d(TAG, "Requesting confirmation");
             // This initiates a prompt dialog for the user to confirm screen projection.
             startActivityForResult(
                     mMediaProjectionManager.createScreenCaptureIntent(),
                     REQUEST_MEDIA_PROJECTION);
+            return false;
         }
     }
 
@@ -261,18 +291,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setUpVirtualDisplay() {
-        mVirtualDisplay = mMediaProjection.createVirtualDisplay("ScreenCapture",
-                mWindowWidth, mWindowHeight, mScreenDensity,
-                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                mImageReader.getSurface(), null, null);
+        if (isScreenshot) {
+            mVirtualDisplay = mMediaProjection.createVirtualDisplay("ScreenCapture",
+                    mWindowWidth, mWindowHeight, mScreenDensity,
+                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    mImageReader.getSurface(), null, null);
+        } else {
+            mVirtualDisplay = mMediaProjection.createVirtualDisplay("record_screen",
+                    mWindowWidth, mWindowHeight, mScreenDensity,
+                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    mSurface, null, null);
+        }
     }
 
     private void stopScreenCapture() {
-        if (mVirtualDisplay == null) {
-            return;
+        if (mVirtualDisplay != null) {
+            mVirtualDisplay.release();
+            mVirtualDisplay = null;
         }
-        mVirtualDisplay.release();
-        mVirtualDisplay = null;
     }
 
     @Override
@@ -300,14 +336,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mResultData = data;
             setUpMediaProjection();
             setUpVirtualDisplay();
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Log.e(TAG, "start startCapture");
-                    startCapture();
-                }
-            }, 1000);
+            Log.e("0525", "onActivityResult 1");
+            if (isScreenshot) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e(TAG, "start startCapture");
+                        startCapture();
+                    }
+                }, 200);
+            } else {
+                recordStart();
+            }
+
+            Log.e("0525", "onActivityResult 2");
         }
     }
 
@@ -319,13 +362,170 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-                    saveToFile();
+                    if (isScreenshot) {
+                        saveToFile();
+                    } else {
+                        recordClick();
+                    }
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    Toast.makeText(this.getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
                 }
                 break;
             }
+        }
+    }
+
+    // record
+    private void configureMedia() {
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", mWindowWidth, mWindowHeight);
+        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 6000000);
+        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 30);
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 2);
+        try {
+            mMediaCodec = MediaCodec.createEncoderByType("video/avc");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mMediaCodec.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        mSurface = mMediaCodec.createInputSurface();
+        mMediaCodec.start();
+    }
+
+    private void recordStart() {
+        Log.e("0525", "recordStart");
+        configureMedia();
+        if (startScreenCapture()) {
+            new Thread() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "start startRecord");
+                    startRecord();
+                }
+            }.start();
+        }
+    }
+
+    private void startRecord() {
+        Log.e("0525", "startRecord");
+        try {
+            mMuxer = new MediaMuxer(mVideoPath + System.currentTimeMillis() + ".mp4", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+            recordVirtualDisplay();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            release();
+        }
+    }
+
+    private void recordVirtualDisplay() {
+        Log.e("0525", "recordVirtualDisplay");
+        while (!mIsQuit.get()) {
+            int index = mMediaCodec.dequeueOutputBuffer(mBufferInfo, 10000);
+            Log.d(TAG, "dequeue output buffer index=" + index);
+            Log.e("0525", "recordVirtualDisplay index=" + index);
+            if (index == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {//后续输出格式变化
+                resetOutputFormat();
+            } else if (index == MediaCodec.INFO_TRY_AGAIN_LATER) {//请求超时
+                Log.d(TAG, "retrieving buffers time out!");
+                Log.e("0525", "recordVirtualDisplay time out");
+                try {
+                    // wait 10ms
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            } else if (index >= 0) {//有效输出
+                if (!mMuxerStarted) {
+                    throw new IllegalStateException("MediaMuxer dose not call addTrack(format) ");
+                }
+                encodeToVideoTrack(index);
+                mMediaCodec.releaseOutputBuffer(index, false);
+            }
+        }
+    }
+
+    private void resetOutputFormat() {
+        Log.e("0525", "resetOutputFormat 1");
+        // should happen before receiving buffers, and should only happen once
+        if (mMuxerStarted) {
+            throw new IllegalStateException("output format already changed!");
+        }
+        MediaFormat newFormat = mMediaCodec.getOutputFormat();
+
+        Log.d(TAG, "output format changed.\n new format: " + newFormat.toString());
+        mVideoTrackIndex = mMuxer.addTrack(newFormat);
+        mMuxer.start();
+        mMuxerStarted = true;
+        Log.e("0525", "resetOutputFormat 2  mMuxer.start();");
+        Log.i(TAG, "started media muxer, videoIndex=" + mVideoTrackIndex);
+    }
+
+    private void encodeToVideoTrack(int index) {
+        Log.e("0525", "encodeToVideoTrack 1");
+        ByteBuffer encodedData = mMediaCodec.getOutputBuffer(index);
+
+        if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {//是编码需要的特定数据，不是媒体数据
+            // The codec config data was pulled out and fed to the muxer when we got
+            // the INFO_OUTPUT_FORMAT_CHANGED status.
+            // Ignore it.
+            Log.d(TAG, "ignoring BUFFER_FLAG_CODEC_CONFIG");
+            mBufferInfo.size = 0;
+        }
+        if (mBufferInfo.size == 0) {
+            Log.d(TAG, "info.size == 0, drop it.");
+            encodedData = null;
+        } else {
+            Log.d(TAG, "got buffer, info: size=" + mBufferInfo.size
+                    + ", presentationTimeUs=" + mBufferInfo.presentationTimeUs
+                    + ", offset=" + mBufferInfo.offset);
+        }
+        if (encodedData != null) {
+            encodedData.position(mBufferInfo.offset);
+            encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
+            mMuxer.writeSampleData(mVideoTrackIndex, encodedData, mBufferInfo);//写入
+            Log.i(TAG, "sent " + mBufferInfo.size + " bytes to muxer...");
+        }
+        Log.e("0525", "encodeToVideoTrack 2");
+    }
+
+    private void recordStop() {
+        mIsQuit.set(true);
+    }
+
+    private void release() {
+        mIsQuit.set(false);
+        mMuxerStarted = false;
+        Log.i(TAG, " release() ");
+        if (mMediaCodec != null) {
+            mMediaCodec.stop();
+            mMediaCodec.release();
+            mMediaCodec = null;
+        }
+        if (mVirtualDisplay != null) {
+            mVirtualDisplay.release();
+            mVirtualDisplay = null;
+        }
+        if (mMuxer != null) {
+            mMuxer.stop();
+            mMuxer.release();
+            mMuxer = null;
+        }
+    }
+
+    private void recordClick() {
+        isRecordOn = !isRecordOn;
+        if (isRecordOn) {
+            Log.e("0525", "Toast record start");
+            Toast.makeText(this.getApplicationContext(), "record start", Toast.LENGTH_SHORT).show();
+            mRecordBtn.setText("RECORDING...");
+            recordStart();
+        } else {
+            Log.e("0525", "Toast record end");
+            Toast.makeText(this.getApplicationContext(), "record end", Toast.LENGTH_SHORT).show();
+            mRecordBtn.setText("RECORD");
+            recordStop();
         }
     }
 }
